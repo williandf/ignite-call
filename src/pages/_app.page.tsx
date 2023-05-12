b364482748c5701294a42b8758c4bf359844a2bc
Willian Del Freu <williankeyex@gmail.com>
2023-05-11 21:37:00 -0300

    Configure SEO

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,6 +4,7 @@ import { globalStyles } from '@/styles/global'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
 
 globalStyles()
 
@@ -14,6 +15,14 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://www.url.io',
+            siteName: 'Ignite Call',
+          }}
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
